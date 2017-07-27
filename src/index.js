2913b7fd1089e58d7bdd3dba2eff01497481f410
john joseph <atiquajohn@gmail.com>
2017-07-27 13:28:35 +0700

    youtube api is complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {Component}from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from "./components/Video_list";
 const API_KEY="AIzaSyAX9d9kTF9vaPeluSHhLDfddaa5QIXJvzA";
 
-const App= () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={videos: []}
+    YTSearch({key: API_KEY, term: "surfaceboad"},(videos)=>{
+      this.setState({videos})
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 ReactDOM.render(<App />, document.querySelector(".container"));
